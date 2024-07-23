5451efab062ab008f6923622a08d3920a1639d87
Pedro Falco <fpfalco@gmail.com>
2024-07-23 19:32:58 -0300

    fix: update static files path

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,7 +28,7 @@
         <slot/>
 
         {#if $page.url.pathname !== '/'}
-            <img class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-96 -z-10 opacity-50" src="/assets/TE_Logo.png" alt="logo bg">
+            <img class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-96 -z-10 opacity-50" src="assets/TE_Logo.png" alt="logo bg">
         {/if}
 
     </main>
